ad9c3863e3d9d851e7b2bcefa9dfffc4d6334d7a
Anslie Pettit <apettit1112@gmail.com>
2026-04-26 18:56:39 -0700

    package.json deployment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 async function startServer() {
   try {
     await sequelize.authenticate();
-    console.log('✅ Database connected');
+    console.log(' Database connected');
 
     app.listen(port, () => {
       console.log(`Server running on http://localhost:${port}`);
